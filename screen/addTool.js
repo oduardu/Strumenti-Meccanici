3b22ff990bf98794eba7156c3a1b640651e6bce1
oduardu <46853580+oduardu@users.noreply.github.com>
2021-11-20 19:43:00 -0300

    PDM - 22/11/2021

--- screen/addTool.js
+++ screen/addTool.js
@@ -3,8 +3,8 @@ import { Text, View, StyleSheet, Platform, Image, TextInput, TouchableOpacity } 
 import { Avatar, Button, Icon } from 'react-native-paper'
 import firebase from 'firebase'
 import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default class addTool extends React.Component {
     constructor(props) {
@@ -13,43 +13,53 @@ export default class addTool extends React.Component {
         const paramsValidos = this.props.route.params != undefined && this.props.route.params.ferramenta != undefined;
 
         this.state = {
-            id: paramsValidos ? this.props.route.params.ferramenta.id : "",
-            nomeFerramenta: paramsValidos ? this.props.route.params.ferramenta.nomeFerramenta : '',
-            disponibilidade: paramsValidos ? this.props.route.params.ferramenta.disponibilidade : '',
-            image: null,
-            hasPermission: null,
+            id: null,
+            nomeFerramenta: null,
+            disponibilidade: null,
+            imagem: null,
             editar: paramsValidos,
+            date: new Date(),
+            mode: 'date',
+            show: false,
+            hasPermission: false
         };
-
     }
-
     async componentDidMount() {
-        this.getPermission();
+        this.getPermissao()
+        this.carrregarDados()
     }
+
+    onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || this.state.date;
+        let dataFormatada = ((currentDate.getDate())) + "/" + ((currentDate.getMonth() + 1)) + "/" + currentDate.getFullYear();
+        this.setState({ show: Platform.OS === 'ios', date: dataFormatada })
+    }
+
+    showMode = (currentMode) => {
+        this.setState({ show: true });
+        this.setState({ mode: currentMode });
+    };
+
+    showDatepicker = () => {
+        this.showMode('date');
+    };
 
     salvar = async () => {
-        let ferramenta = {
+        let tools = {
             id: this.state.id,
             nomeFerramenta: this.state.nomeFerramenta,
-            disponibilidade: this.state.disponibilidade,
-            imagem: this.state.image,
-        };
-
-        if (ferramenta.imagem) {
-
-            ferramenta.imagem = await this.uploadImage(this.state.image, "ferramenta");
+            disponibilidade: this.state.date,
+            imagem: this.state.imagem,
         }
+    
+        if (tools.id != null) {
 
-        if (ferramenta.id != null) {
-
-            console.log('id:' + ferramenta.id);
-
-            var ferramentaRef = firebase.database().ref("ferramenta").child(ferramenta.id);
+            var ferramentaRef = firebase.database().ref("ferramenta/" + tools.id);
 
             ferramentaRef.update({
-                nomeFerramenta: ferramenta.nomeFerramenta,
-                disponibilidade: ferramenta.disponibilidade,
-                imagem: ferramenta.url,
+                nomeFerramenta: tools.nomeFerramenta,
+                disponibilidade: tools.disponibilidade,
+                imagem: tools.imagem
             }).then(() => {
                 console.log("Atualizado");
             }).catch((error) => {
@@ -57,85 +67,111 @@ export default class addTool extends React.Component {
             });
 
         } else {
+            var ferramentaRef = firebase.database().ref("ferramenta");
 
-            console.log('criando novo');
-
-            firebase.database().ref("ferramenta").push({
-                nomeFerramenta: ferramenta.nomeFerramenta,
-                disponibilidade: ferramenta.disponibilidade,
-                imagem: ferramenta.url,
+            ferramentaRef.push({
+                nomeFerramenta: tools.nomeFerramenta,
+                disponibilidade: tools.disponibilidade,
+                imagem: tools.imagem
             }).then(() => {
+                console.log("inserido");
             }).catch((error) => {
                 console.log(error);
             });
         }
+
         this.setState({
             nomeFerramenta: null,
-            disponibilidade: null,
-            url: null,
+            telefoneMecanico: null,
+            imagem: null,
+
         });
+        this.props.navigation.navigate("toolList")
+    }
 
-        this.props.navigation.navigate("Listagem Ferramenta");
-    };
-
-    async selecionarImagem() {
+    async escolherImagem() {
         this.getPermissao();
         if (this.state.hasPermission) {
-          let resultado = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-          });
-    
-          if (!resultado.cancelled) {
-            await this.setState({ image: result.uri });
-          }
-        }
-      }
-    
-      uploadImage = async (uri, tipoImagem) => {
-        try {
-            const response = await fetch(uri);
-            const blob = await response.blob(uri);
+            let resultado = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [1, 1],
+                quality: 1,
+                base64: true
+            });
 
-            var now = new Date();
-
-            var datetime = now.getFullYear() + '_' + (now.getMonth() + 1) + '_' + now.getDate();
-            datetime += '_' + now.getHours() + '_' + now.getMinutes() + '_' + now.getSeconds();
-
-            let path = "imagem/" + tipoImagem + '_' + datetime + ".jpg";
-
-            return new Promise((res, rej) => {
-                firebase.storage().ref()
-                    .child(path)
-                    .put(blob)
-                    .then((snapshot) => {
-                        snapshot.ref.getDownloadURL().then((downloadUrl) => {
-                            res({ uri: downloadUrl, path: path })
-                        })
-                    })
-                    .catch((error) => {
-                        rej(error)
-                    })
-            })
-        }
-        catch (error) {
-            console.error(error);
+            if (!resultado.cancelled) {
+                await this.setState({ imagem: resultado.base64 });
+            }
         }
     }
 
-    getPermission = async () => {
-        if (Platform.OS === 'ios') {
-            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    // uploadImage = async (uri, tipoImagem) => {
+    //     try {
+    //         const response = await fetch(uri);
+    //         const blob = await response.blob(uri);
+
+    //         // let now = new Date();
+
+    //         // let datetime = now.getFullYear() + '_' + (now.getMonth() + 1) + '_' + now.getDate();
+    //         // datetime += '_' + now.getHours() + '_' + now.getMinutes() + '_' + now.getSeconds();
+
+    //         // let path = "imagem/" + datetime;
+
+    //         // return new Promise((res, rej) => {
+    //         //     firebase.storage().ref()
+    //         //         .child(path)
+    //         //         .put(blob)
+    //         //         .then((snapshot) => {
+    //         //             snapshot.ref.getDownloadURL().then((downloadUrl) => {
+    //         //                 res({ uri: downloadUrl, path: path })
+    //         //             })
+    //         //         })
+    //         //         .catch((error) => {
+    //         //             rej(error)
+    //         //         })
+    //         // })
+
+    //     }
+    //     catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    async getPermissao() {
+        if (Platform.OS === "ios") {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
             if (status !== 'granted') {
-                alert('Hey! You might want to enable gallery permissions for my app, they are good.');
+                alert('Por favor, aceite as permissões!')
             }
         }
-        this.setState({ hasPermission: status === "granted" });
-    };
+        const { status } = await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({ hasPermission: status === 'granted' })
+    }
+
+    carrregarDados = () => {
+        let { route } = this.props;
+        if (route.params != null) {
+            this.setState({
+                id: route.params.ferramenta.id,
+                nomeFerramenta: route.params.ferramenta.nomeFerramenta,
+                imagem: route.params.ferramenta.imagem,
+            });
+        }
+    }
 
     render() {
+        
+        const dateTimerPickerView =
+            <DateTimePicker
+                testID="dateTimePicker"
+                value={this.state.date}
+                mode={this.state.mode}
+                is24Hour={true}
+                display="default"
+                onChange={this.onChange}
+            />
+
         return (
             <View style={styles.container}>
 
@@ -150,83 +186,70 @@ export default class addTool extends React.Component {
                     <Text style={{ fontSize: 20, color: '#A5BBFF' }}>
                         Formulario Ferramenta
                     </Text>
+                </View>
+                <TouchableOpacity style={{
+                    alignItems: 'center',
+                    marginLeft: '30%',
+                    marginBottom: 30,
+                    width: '40%',
+                    height: 200,
+                }}
+                    onPress={() => this.escolherImagem()}
+                >
+                    <Image style={styles.imagem} source={{
+                        uri: this.state.imagem == null || this.state.imagem == undefined ? 'https://cdn-icons-png.flaticon.com/512/2983/2983067.png' : 'data:image/png;base64,' + this.state.imagem
+                    }} />
+                    <View style={{
+                        paddingHorizontal: 30,
+                        marginBottom: 10,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginTop: 15
+                    }}>
+
                     </View>
-                        <TouchableOpacity style={{
-                            alignItems: 'center',
-                            marginLeft: '30%',
-                            marginBottom: 30,
-                            width: '40%',
-                            height: 200,
+                </TouchableOpacity>
+                <View style={{ alignItems: 'center' }}>
+                    <TextInput
+                        dense='true'
+                        mode='outlined'
+                        placeholder="Nome da Ferramenta"
+                        style={{
+                            borderRadius: 10,
+                            borderWidth: 2,
+                            backgroundColor: '#fff',
+                            padding: 18,
+                            textAlignVertical: 'top',
+                            width: 300,
+                            borderColor: '#B98EFF',
+                            marginBottom: 20,
                         }}
-                            onPress={() => this.selecionarImagem()}
+                        value={this.state.nomeFerramenta}
+                        onChangeText={(text) => this.setState({ nomeFerramenta: text })}
+                    />
+                    {this.state.show ? dateTimerPickerView : null}
+                    <View>
+                        <Button
+                            color='#FFF'
+                            style={{ borderRadius: 20, marginTop: 5, width: 300, backgroundColor: '#B98EFF' }}
+                            onPress={() => this.showDatepicker()}
                         >
-                            <Image
-                                source={{
-                                    uri: this.state.url == null || this.state.url == undefined ? 'https://cdn-icons-png.flaticon.com/512/675/675579.png' : 'data:image/png;base64,'+this.state.url
-                                }}
-                                style={{
-                                    marginTop: 2,
-                                    width: 200,
-                                    height: 200,
-                                    borderRadius: 100,
-                                    marginBottom: '2%',
-                                    opacity: 0.8,
-                                }
-                                }
-                            />
-                            <View style={{
-                                paddingHorizontal: 30,
-                                marginBottom: 10,
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                marginTop: 15
-                            }}>
-                                
-                            </View>
-                            </TouchableOpacity>
-                    <View style={{ alignItems: 'center' }}>
-                        <TextInput
-                            dense='true'
-                            mode='outlined'
-                            placeholder="Nome da Ferramenta"
-                            style={{
-                                borderRadius: 10,
-                                borderWidth: 2,
-                                backgroundColor: '#fff',
-                                padding: 18,
-                                textAlignVertical: 'top',
-                                width: 300,
-                                borderColor: '#B98EFF',
-                                marginBottom: 20,
-                            }}
-                            value={this.state.nomeFerramenta}
-                            onChangeText={(text) => this.setState({ nomeFerramenta: text })}
-                        />
-                        <TextInput
-                            dense='true'
-                            mode='outlined'
-                            placeholder="Disponibilidade"
-                            style={{
-                                borderRadius: 10,
-                                borderWidth: 2,
-                                backgroundColor: '#fff',
-                                padding: 18,
-                                textAlignVertical: 'top',
-                                width: 300,
-                                borderColor: '#B98EFF',
-                                marginBottom: 20,
-                            }}
-                            value={this.state.disponibilidade}
-                            onChangeText={(text) => this.setState({ disponibilidade: text })}
-                        />
-                    </View>
-                    <View style={{ alignItems: 'center' }}>
-                        <Button style={{ borderRadius: 20, marginTop: 5, width: 300, backgroundColor: '#B98EFF' }} mode="contained" onPress={() => this.salvar()}>
-                            Salvar
+                            Selecionar Disponibilidade
                         </Button>
                     </View>
+                    <Text style={{
+                    fontSize: 20,
+                    color: '#B98EFF'
+                }}>{this.state.date ? dataFormatada : ""}</Text>
+
                 </View>
+                <View style={{ alignItems: 'center' }}>
+                    <Button style={{ borderRadius: 20, marginTop: 5, width: 300, backgroundColor: '#B98EFF' }} mode="contained" onPress={() => this.salvar()}>
+                        Salvar
+                    </Button>
+                </View>
+            </View>
         )
     }
 }
@@ -244,6 +267,13 @@ const styles = StyleSheet.create({
         padding: 18,
         textAlignVertical: 'top',
         width: 300,
+    },
+    imagem: {
+        marginTop: 2,
+        width: 200,
+        height: 200,
+        marginBottom: '2%',
+        opacity: 0.8,
     },
 
     topText: {
